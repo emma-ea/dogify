d1513d6dbba21d111217bba178dcb1cf94a5c532
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-23 00:19:06 +0000

    provide modules for koin

--- shared/src/commonMain/kotlin/com/emma_ea/dogify/di/KoinModule.kt
+++ shared/src/commonMain/kotlin/com/emma_ea/dogify/di/KoinModule.kt
@@ -1,8 +1,12 @@
 package com.emma_ea.dogify.di
 
+import com.emma_ea.dogify.api.BreedsApi
 import com.emma_ea.dogify.model.FetchBreedUsecase
 import com.emma_ea.dogify.model.GetBreedsUsecase
 import com.emma_ea.dogify.model.ToggleFavouriteStateUsecase
+import com.emma_ea.dogify.repository.BreedsRemoteSource
+import com.emma_ea.dogify.repository.BreedsRepository
+import com.emma_ea.dogify.utils.getDispatcherProvider
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
@@ -13,7 +17,22 @@ private val useCaseModule = module {
     factory {ToggleFavouriteStateUsecase() }
 }
 
-private val sharedModules = listOf(useCaseModule)
+private val repositories = module {
+    single { BreedsRepository(get()) }
+    factory { BreedsRemoteSource(get(), get()) }
+}
+
+private val apiModule = module {
+    factory { BreedsApi() }
+}
+
+private val utilityModule = module {
+    factory { getDispatcherProvider() }
+}
+
+private val sharedModules = listOf(
+    useCaseModule, repositories, apiModule, utilityModule
+)
 
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
     startKoin {
