57294e0c36963367df563356e8655674ebf358c0
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-23 11:09:48 +0000

    setup viewmodel for compose ui

---
+++ shared/src/androidMain/kotlin/com/emma_ea/dogify/viewmodel/MainViewModel.kt
@@ -0,0 +1,101 @@
+package com.emma_ea.dogify.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.emma_ea.dogify.model.Breed
+import com.emma_ea.dogify.model.FetchBreedUsecase
+import com.emma_ea.dogify.model.GetBreedsUsecase
+import com.emma_ea.dogify.model.ToggleFavouriteStateUsecase
+import com.emma_ea.dogify.repository.BreedsRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainViewModel(
+    breedsRepository: BreedsRepository,
+    private val getBreeds: GetBreedsUsecase,
+    private val fetchBreeds: FetchBreedUsecase,
+    private val onToggleFavouriteState: ToggleFavouriteStateUsecase
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(State.LOADING)
+    val state: StateFlow<State> = _state
+
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing: StateFlow<Boolean> = _isRefreshing
+
+    private val _events = MutableSharedFlow<Event>()
+    val events: SharedFlow<Event> = _events
+
+    private val _shouldFilterFavourites = MutableStateFlow(false)
+    val shouldFilterFavourites: StateFlow<Boolean> = _shouldFilterFavourites
+
+    val breeds = breedsRepository.breeds.combine(shouldFilterFavourites) {
+            breeds, shouldFilterFavourites ->
+            if (shouldFilterFavourites) {
+                breeds.filter { it.isFavourite }
+            }else {
+                breeds
+            }.also {
+                _state.value = if (it.isEmpty()) State.EMPTY else State.NORMAL
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        emptyList()
+    )
+
+    init {
+        loadData()
+    }
+
+    fun refresh() {
+        loadData(true)
+    }
+
+    private fun loadData(isForceRefresh: Boolean = false) {
+        val getData: suspend () -> List<Breed> =
+            {
+                if (isForceRefresh) fetchBreeds.invoke()
+                else getBreeds.invoke()
+            }
+
+        if (isForceRefresh) {
+            _isRefreshing.value = true
+        } else {
+            _state.value = State.LOADING
+        }
+
+        viewModelScope.launch {
+            _state.value = try {
+                getData()
+                State.NORMAL
+            } catch (e: Exception) {
+                State.ERROR
+            }
+            _isRefreshing.value = false
+        }
+    }
+
+    fun onToggleFavouriteFilter() {
+        _shouldFilterFavourites.value = !_shouldFilterFavourites.value
+    }
+
+    fun onFavouriteTapped(breed: Breed) {
+        viewModelScope.launch {
+            try {
+                onToggleFavouriteState(breed)
+            } catch (e: Exception) {
+                _events.emit(Event.ERROR)
+            }
+        }
+    }
+
+}
+
+enum class State {
+    LOADING, NORMAL, ERROR, EMPTY
+}
+
+enum class Event {
+    ERROR
+}
