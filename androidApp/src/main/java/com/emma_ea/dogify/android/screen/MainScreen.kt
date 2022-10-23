7824a1f266bcb0b97e2af5266db6736bfcebd5e8
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-23 18:20:08 +0000

    impl scaffold with swipeRefresh

--- androidApp/src/main/java/com/emma_ea/dogify/android/screen/MainScreen.kt
+++ androidApp/src/main/java/com/emma_ea/dogify/android/screen/MainScreen.kt
@@ -1,12 +1,28 @@
 package com.emma_ea.dogify.android
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 import com.emma_ea.dogify.android.viewmodel.MainViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import java.util.*
 
 @Composable
 fun MainScreen(
@@ -21,7 +37,38 @@ fun MainScreen(
 
     val scaffoldState = rememberScaffoldState()
     val snackbarCoroutineScope = rememberCoroutineScope()
-    
-    Text(text = "Hello, Compose")
 
+    Scaffold(scaffoldState = scaffoldState) {
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(isRefreshing = isRefreshing),
+            onRefresh = viewModel::refresh
+        ) {
+            
+        }
+    }
+
+}
+
+@Composable
+fun CustomText(
+    text: String
+) {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Blue,
+                    fontSize = 48.sp,
+                    fontStyle = FontStyle.Italic
+                )
+            ) {
+                append(text[0].uppercase())
+            }
+            withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                append(text.substring(1))
+            }
+        },
+        color = Color.DarkGray,
+        fontSize = 30.sp
+    )
 }
