9b45e906b1f50ec04b19b265008945429d320b1d
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-10-23 11:11:35 +0000

    register viewmodel module

--- shared/src/androidMain/kotlin/com/emma_ea/dogify/DogifyApplication.kt
+++ shared/src/androidMain/kotlin/com/emma_ea/dogify/DogifyApplication.kt
@@ -2,6 +2,7 @@ package com.emma_ea.dogify
 
 import android.app.Application
 import com.emma_ea.dogify.di.initKoin
+import com.emma_ea.dogify.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 
 class DogifyApplication : Application() {
@@ -9,6 +10,7 @@ class DogifyApplication : Application() {
         super.onCreate()
         initKoin {
             androidContext(this@DogifyApplication)
+            modules(viewModelModule)
         }
     }
 }
